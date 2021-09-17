f277e1729e171321c93ee53ee5e45f2b06cb6630
Mateusz Paprocki <mattpap@gmail.com>
2021-09-17 13:33:20 +0200

    Update custom extensions of label annotation

--- sphinx/source/docs/user_guide/examples/extensions_example_latex.py
+++ sphinx/source/docs/user_guide/examples/extensions_example_latex.py
@@ -3,51 +3,20 @@
 
 import numpy as np
 
-from bokeh.models import Label
+from bokeh.models.annotations.html import Label
 from bokeh.plotting import figure, show
 from bokeh.util.compiler import TypeScript
 
 TS_CODE = """
 import * as p from "core/properties"
-import {Label, LabelView} from "models/annotations/label"
+import {Label, LabelView} from "models/annotations/html/label"
 declare const katex: any
 
 export class LatexLabelView extends LabelView {
   model: LatexLabel
 
-  render(): void {
-    //--- Start of copied section from ``Label.render`` implementation
-
-    // Here because AngleSpec does units tranform and label doesn't support specs
-    let angle: number
-    switch (this.model.angle_units) {
-      case "rad": {
-        angle = -this.model.angle
-        break
-      }
-      case "deg": {
-        angle = (-this.model.angle * Math.PI) / 180.0
-        break
-      }
-      default:
-        throw new Error("unreachable code")
-    }
-
-    const panel = this.layout ?? this.plot_view.layout.center_panel
-
-    let sx = this.model.x_units == "data" ? this.coordinates.x_scale.compute(this.model.x) : panel.xview.compute(this.model.x)
-    let sy = this.model.y_units == "data" ? this.coordinates.y_scale.compute(this.model.y) : panel.yview.compute(this.model.y)
-
-    sx += this.model.x_offset
-    sy -= this.model.y_offset
-
-    //--- End of copied section from ``Label.render`` implementation
-    // Must render as superpositioned div (not on canvas) so that KaTex
-    // css can properly style the text
-    this._css_text(this.layer.ctx, "", sx, sy, angle)
-
-    // ``katex`` is loaded into the global window at runtime
-    // katex.renderToString returns a html ``span`` element
+  protected override _render(): void {
+    super._render()
     katex.render(this.model.text, this.el, {displayMode: true})
   }
 }
@@ -63,6 +32,8 @@ export interface LatexLabel extends LatexLabel.Attrs {}
 export class LatexLabel extends Label {
   properties: LatexLabel.Props
   __view_type__: LatexLabelView
+
+  static __module__ = "latex_label"
 
   constructor(attrs?: Partial<LatexLabel.Attrs>) {
     super(attrs)
@@ -84,6 +55,7 @@ class LatexLabel(Label):
     """
     __javascript__ = ["https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.js"]
     __css__ = ["https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css"]
+    __view_module__ = "latex_label"
     __implementation__ = TypeScript(TS_CODE)
 
 
